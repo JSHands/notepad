ba6d7848af0515918bc0ca29ad4dbd851a4cafc1
DecaHub <dan@decahub.com>
2016-09-28 16:22:51 -0400

    Complete CRUD operations on Client

--- app/components/editor/editor.controller.js
+++ app/components/editor/editor.controller.js
@@ -6,7 +6,6 @@
 		
 		$scope.editing = true;
 		$scope.notes = [];
-		$scope.test = "lol";
 		
 		$scope.view = (index) => {
 			$scope.editing = false;
@@ -22,16 +21,48 @@
 		};
 		
 		$scope.save = () => {
+			
+			console.log("Saving note...");
+			
 			$scope.content.date = new Date();
 			
 			if ($scope.content.id) {
-				$http.put('http://localhost:5000/notes/' + $scope.content.id, $scope.content).success((data) => {
+				$http.put('http://localhost:5000/notes/' +
+					$scope.content.id, $scope.content).success((data) => {
 					$scope.editing = false;
 				}).error((err) => {
 					$scope.error = 'Could not update note';
 				})
+			} else {
+				$scope.content.id = Date.now();
+				$http.post('http://localhost:5000/notes', $scope.content).success((data) => {
+					$scope.notes.push($scope.content);
+					$scope.editing = false;
+				}).error((err) => {
+					$scope.error = 'Could not create note';
+				})
 			}
 			
+		};
+		
+		$scope.remove = () => {
+			$http.delete('http://localhost:5000/notes/' + $scope.content.id).success((data) => {
+				let index = data.index;
+				
+				console.log("INDEX TO BE DELETED: " + index);
+				
+				if (index > -1) {
+					$scope.notes.splice(index,  1);
+				}
+				
+				$scope.content = {
+					title: '',
+					content: ''
+				};
+				
+			}).error((err) => {
+				$scope.error = 'Could not delete note.';
+			})
 		};
 		
 		$http.get('http://localhost:5000/notes').
